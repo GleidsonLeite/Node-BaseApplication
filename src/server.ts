a78f7bae49d1394d49d980a60276014543bb513c
Gleidson Leite <gledson.leytte@hotmail.com>
2021-03-01 23:10:39 -0300

    Typescript configured

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express, {Request, Response} from 'express'
+
+const app = express();
+
+app.get('/', (request:Request, response:Response) => {
+  return response.json({message: 'Hello World'})
+})
+
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!')
+})
+
