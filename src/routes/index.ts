b3a762e58481ff8012bba1f87a4225dbf585b904
Gleidson Leite <gledson.leytte@hotmail.com>
2021-03-01 23:27:51 -0300

    Fixing some typescript issues

---
+++ src/routes/index.ts
@@ -0,0 +1,6 @@
+import { Router, Request, Response } from 'express';
+
+const routes = Router();
+routes.get('/', (request:Request, response:Response) => response.json({ message: 'Hello World' }));
+
+export default routes;
